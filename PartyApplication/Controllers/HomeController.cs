32beb395c13722d688102a40dcd5aac030db45a3
Ryan Demboski <ryandemboski7@gmail.com>
2021-10-22 15:56:30 -0700

    Added project from master to main

---
+++ PartyApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        [Route("")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
