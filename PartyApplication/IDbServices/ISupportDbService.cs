11d274ff9258a1815687fde841f09f314d8f8ccd
Shayne Sellner <sps266@nau.edu>
2021-10-23 13:05:01 -0700

    Added the first iteration of the Support System

---
+++ PartyApplication/IDbServices/ISupportDbService.cs
@@ -0,0 +1,13 @@
+﻿using PartyApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyApplication.IDbServices
+{
+    interface ISupportDbService
+    {
+        Task AddSupportAsync(Support item);
+    }
+}
