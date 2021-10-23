96137221a0202dd258547bc1bc7c9ee0e6705824
Shayne Sellner <sps266@nau.edu>
2021-10-23 15:06:01 -0700

    Implemented the support system

--- PartyApplication/DbServices/SupportDbService.cs
+++ PartyApplication/DbServices/SupportDbService.cs
@@ -27,7 +27,7 @@ namespace PartyApplication.DbServices
             {
                 try
                 {
-                    await this._container.CreateItemAsync<Support>(item, new PartitionKey(item.id.ToString()));
+                    await this._container.CreateItemAsync<Support>(item, new PartitionKey(item.Id.ToString()));
                 }
                 catch (Exception ex)
                 {
